a74e569ed3f104f3fb6812c4a4e0ae63458dd0e6
devJm95 <leehopl2@gmail.com>
2025-07-10 10:34:46 +0900

    feat: 메인 화면에 슬라이더 넘기는 기능 구현

--- src/components/SwiperNavButton.tsx
+++ src/components/SwiperNavButton.tsx
@@ -8,13 +8,13 @@ const SwiperNavButton = () => {
     <div className="absolute top-1/2 right-0 left-0 z-10 flex -translate-y-1/2 justify-between px-[2rem] lg:px-[5rem]">
       <button
         onClick={() => swiper.slidePrev()}
-        className="z-10 flex h-[5rem] w-[5rem] items-center justify-center rounded-full bg-[#1C1C1C] text-white"
+        className="z-[9999] cursor-pointer flex h-[5rem] w-[5rem] items-center justify-center rounded-full bg-[#1C1C1C] text-white"
       >
         <ArrowLeft className="h-12 w-12" />
       </button>
       <button
         onClick={() => swiper.slideNext()}
-        className="z-10 flex h-[5rem] w-[5rem] items-center justify-center rounded-full bg-[#1C1C1C] text-white"
+        className="z-[9999] cursor-pointer flex h-[5rem] w-[5rem] items-center justify-center rounded-full bg-[#1C1C1C] text-white"
       >
         <ArrowRight className="h-12 w-12" />
       </button>
