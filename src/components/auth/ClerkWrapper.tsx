bce18aa93b43fa9ed35df88e9d89ed8e7aa7f162
devJm95 <leehopl2@gmail.com>
2025-07-28 10:34:16 +0900

    feat: Clerk 세션 생성 및 로그인 처리 주체 추가

--- src/components/auth/ClerkWrapper.tsx
+++ src/components/auth/ClerkWrapper.tsx
@@ -4,5 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ReactNode } from 'react'
 
 export default function ClerkWrapper({ children }: { children: ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>
+  return <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+          {children}
+         </ClerkProvider>
 }
